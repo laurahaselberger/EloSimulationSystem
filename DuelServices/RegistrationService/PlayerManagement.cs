aef153dcca5f271b7f14d9b3f1936f1ae397aefc
Aylin <a.schlager@htlkrems.at>
2023-11-14 10:03:55 +0100

    idk

---
+++ DuelServices/RegistrationService/PlayerManagement.cs
@@ -0,0 +1,35 @@
+﻿namespace RegistrationService;
+
+public class PlayerManagement
+{
+    public List<Player> Players { get; set; }
+
+    public Player Registration() 
+    {
+        /*
+            Anlegen eines neuen Spielers
+            Name wird im Body der Request übergeben
+            Id & Elo-Wert wird vom Service vergebn
+        */
+        Player p = new Player();
+        return p;
+    }
+    public Player Update(int id)
+    {
+        /*
+         * Aktualisiert einen vorhanden Spieler
+         * Name & Elo-Wert können aktualisiert werden --> niemals die Id
+         */
+        Player p = new Player();
+        return p;
+    }
+
+    public Player GetAllPlayer()
+    {
+        /*
+         * liefert die Liste aller Spieler zurück
+         */
+        Player p = new Player();
+        return p;
+    }
+}
